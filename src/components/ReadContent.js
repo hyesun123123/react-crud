0e593ec9471394538a942ca58839740312efa338
hyesun123123 <goodnight4857@naver.com>
2021-06-13 03:27:02 +0900

    메시지내용

---
+++ src/components/ReadContent.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class ReadContent extends Component{
+    
+    render(){
+        console.log('read content render!');
+        return(
+            <article>
+                <h2>
+                    {this.props.title}
+                </h2>
+                {this.props.desc}
+            </article>
+        )
+    }
+}
+export default ReadContent;
